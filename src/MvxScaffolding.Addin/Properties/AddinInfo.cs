b482a7ba40d2e4daf1a354129fab908bf71fcaf3
Plac3hold3r <Plac3hold3r+github@outlook.com>
2018-04-09 20:59:12 +0200

    Add solution for mac addin

---
+++ src/MvxScaffolding.Addin/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "MvxScaffolding_Addin",
+    Namespace = "MvxScaffolding_Addin",
+    Version = "1.0"
+)]
+
+[assembly: AddinName("MvxScaffolding_Addin")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("MvxScaffolding_Addin")]
+[assembly: AddinAuthor("Plac3hold3r")]
